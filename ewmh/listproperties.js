49b06c40847766fb1cc6fe22723e2c82ffd4a754
Ellis Whitehead <ellis@ellisw.net>
2015-09-25 15:56:29 +0200

    Fix ATOMs

--- ewmh/listproperties.js
+++ ewmh/listproperties.js
@@ -14,6 +14,7 @@ function getWindowProperty(X, wid, propertyId) {
 }
 
 function getAtomName(X, propertyId) {
+	//console.log("getAtomName("+propertyId+")");
 	return new Promise((resolve, reject) => {
 		X.GetAtomName(propertyId, function(err, result) {
 			if (err) return reject(err);
@@ -24,10 +25,18 @@ function getAtomName(X, propertyId) {
 
 function bytesToString(bytes, encoding='utf8') {
 	let buffer = new Buffer(bytes);
-	console.log("buffer: "+buffer);
+	//console.log("buffer: "+buffer);
 	let s = buffer.toString(encoding);
-	console.log("s: "+s);
+	//console.log("s: "+s);
 	return _.compact(new Buffer(bytes).toString(encoding).split('\u0000'))
+}
+
+function bytesToNumbers(bytes) {
+	var res = [];
+	for (var i=0; i < bytes.length; i+=4) {
+		res.push(bytes.unpack('L', i)[0]);
+	}
+	return res;
 }
 
 x11.createClient((err, display) => {
@@ -38,21 +47,18 @@ x11.createClient((err, display) => {
 		return new Promise((resolve, reject) => {
 			switch(type) {
 				case 'ATOM':
-					var numAtoms = data.length/4;
-					var res = [];
-					for (var i=0; i < data.length; i+=4) {
-						var a = data.unpack('L', i)[0];
-						res.push(getAtomName(X, a));
-					}
-					return new Promise.all(res);
+					const ns = bytesToNumbers(data);
+					//console.log("ATOM data: "+_.map(data, function(c) { return c.toString(); }));
+					//console.log("ATOM ns: "+ns);
+					const promises = _.map(ns, n => { return getAtomName(X, n); });
+					resolve(Promise.all(promises));
+
+				case 'CARDINAL':
+					//return resolve([]);
+					return resolve(bytesToNumbers(data));
 
 				case 'INTEGER':
-					var numAtoms = data.length/4;
-					var res = [];
-					for (var i=0; i < data.length; i+=4) {
-						res.push(data.unpack('L', i)[0]);
-					}
-					return resolve(res);
+					return resolve(bytesToNumbers(data));
 
 				case 'STRING':
 					return resolve(bytesToString(data, 'ascii'));
@@ -67,6 +73,12 @@ x11.createClient((err, display) => {
 						res.push(data.unpack('L', i)[0]);
 					}
 					return resolve(res);
+
+				case 'WM_HINTS':
+					return resolve(bytesToNumbers(data));
+
+				case 'WM_STATE':
+					return resolve(bytesToNumbers(data));
 
 				default:
 					return resolve('???:' + type+":"+_.map(data, function(c) { return c.toString(); }));
@@ -92,30 +104,30 @@ x11.createClient((err, display) => {
     });
 	*/
 	X.ListProperties(id, (err, props) => {
-		console.log("A:"+props);
+		//console.log("A:"+props);
 		const promises = props.map(p => {
-			console.log(p);
+			//console.log(p);
 			return getWindowProperty(X, id, p).then(propValue => {
-				console.log("B: pid="+p);
+				//console.log("B: pid="+p);
 				return getAtomName(X, propValue.type).then(typeName => {
-					console.log("C: pid="+p);
+					//console.log("C: pid="+p);
 					return getAtomName(X, p).then(propName => {
-						console.log("D: pid="+p);
+						//console.log("D: pid="+p);
 						return decodeProperty(typeName, propValue.data).then(decodedData => {
-							console.log("decoded: "+decodedData);
+							//console.log("decoded: "+decodedData);
 							//console.log(p + ': ' + propName + '(' + typeName + ') = ' + decodedData);
 							return [propName, decodedData];
-						}).catch(e => {console.log("ERROR: "+e) });
+						}).catch(e => {console.log("ERROR: "+e.name + ': ' + e.message); console.log(e.stack); });
 					});
 				});
 			});
 		});
 
-		console.log("B:");
+		//console.log("B:");
 		Promise.all(promises).then(pairs => {
-			console.log("pairs:");
-			console.log(pairs);
-			console.log("YO!!!!!!!!!!!");
+			//console.log("pairs:");
+			//console.log(pairs);
+			//console.log("YO!!!!!!!!!!!");
 			const obj = _.zipObject(pairs);
 			console.log(JSON.stringify(obj, null, '\t'));
 			//pairs.forEach(s => { console.log("|"+s); });
